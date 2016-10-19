9c00b38e0fe409e72b5726c0a42d97e0fb9971fc
Ning Yuan <Ning Yuan>
2016-10-19 13:00:00 +0800

    Fixed universal mode! TODO: Add reddit mode.

--- main.py
+++ main.py
@@ -2,41 +2,44 @@ import requests
 import bs4
 
 
-# gets URL from input
-def getURL():
-    url = input("Enter URL here >>>")
-    return url
-
-
 # gets soup object from url
-def getSoup(url):
+def genSoup(url):
     html = requests.get(url)
-    print("Retrieving .html..."),
     soup = bs4.BeautifulSoup(html.text, 'html.parser')
-    print("Parsing soup object..."),
     return soup
 
 
 # universal retrieval
-def allGet(url, soup):
-    print("Switching to universal mode..."),
-    imgtags = soup.select('img')
-    print("Building image URLs...")
+def getAll(url, soup):
+    preimgsrc = []
     imgsrc = []
+    imgtags = soup.select('img')
     for tag in imgtags:
-        if 'http' in tag.get('src'):
-            src = tag.get('src')
-        else:
-            src = 'http:' + tag.get('src')
-            imgsrc.append(src)
+        preimgsrc.append(tag.get('src'))
+    # TODO try this with re
+    domain = url[url.find('/') + 2:]
+    domain = domain[:domain.find('/')]
+    # '''
+    atags = soup.select('a')
+    for tag in atags:
+        if type(tag.get('href')) is str:
+            # TODO expand selection criteria with ext list below
+            if ('.jpg' or '.png' or '.jpeg') in tag.get('href'):
+                preimgsrc.append(tag.get('href'))   
+    # '''
+    for imgurl in preimgsrc:
+        if ('http' or 'https') in imgurl:
+            imgsrc.append(imgurl)
+        elif imgurl[:2] == '//':
+            imgsrc.append('http:' + imgurl)
+        elif imgurl[0] == '/' and imgurl[1] != '/':
+            imgsrc.append(domain + imgurl)
     return imgsrc
 
 
 # retrieves imgsrc list of img URLS for 4chan
-def chanGet(url, soup):
-    print("Switching to 4chan mode..."),
+def getChan(url, soup):
     fileText = soup.select('div .fileText a')
-    print("Building image URLs..."),
     imgsrc = []
     for tag in fileText:
         src = 'http:' + tag.get('href')
@@ -55,7 +58,6 @@ def cleanName(filename):
 
 # iterates over list to download images
 def download(imgsrc):
-    print("Starting downloads...")
     dlcount = 1
     for imgurl in imgsrc:
         print("Downloading", str(dlcount), "of", str(len(imgsrc)))
@@ -68,15 +70,19 @@ def download(imgsrc):
             imgfile.write(chunk)
         imgfile.close()
         dlcount += 1
-    print("All downloads complete!")
+
 
 # TODO make a downloadAll function for ease of use when importing this module
+
 if __name__ == '__main__':
-    url = getURL()
-    soup = getSoup(url)
+    url = input('URL >>> ')
+    soup = genSoup(url)
+    print('Generating soup object... '),
     if '4chan' in url:
-        imgsrc = chanGet(url, soup)
+        print("Downloading with '4chan' function...")
+        imgsrc = getChan(url, soup)
     else:
-        imgsrc = allGet(url, soup)
+        print("Downloading with universal function...")
+        imgsrc = getAll(url, soup)
     download(imgsrc)
-    input("Press enter to exit.")
+    input("Done! Enter to exit.")
