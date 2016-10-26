1a3eb3aeba16ca5b7768d2cdb65fb2670ee1ee7b
ningSP4 <ningyuan.sg@gmail.com>
2016-10-26 23:26:31 +0800

    Changed syntax for write file to PEP 8 standard.

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-import requests
-import bs4
+# imgDL is a python-CLI script to download images off webpages
+# Inspired by youtube-dl
+# ningyuan.sg@gmail.com, ningyuan.io
 
+import sys, os, time
+try:
+    import requests
+except ImportError:
+    sys.stderr.write("ERROR: requests is not installed. This is a requirement for imgDL.\n")
+    sys.stderr.flush()
+    time.sleep(2)
+    exit()
+try:
+    import bs4
+except ImportError:
+    sys.stderr.write("ERROR: bs4 is not installed. This is a requirement for imgDL.\n")
+    sys.stderr.flush()
+    time.sleep(2)
+    exit()
+try:
+    import selenium.webdriver as webdriver
+except ImportError:
+    sys.stderr.write("ERROR: Selenium is not installed. This is a requirement for imgur links.")
+    sys.stderr.flush()
+    time.sleep(2)
+
+# TODO: Clean the code, one function for each site
+# TODO: allow sys.argv
+# TODO: fix errors with unreliable web links, conditionals need to be more strict
+# TODO: change file name, or at least main.exe to imgDL.exe
 
 # gets soup object from url
-def genSoup(url):
+def getSoup(url):
     html = requests.get(url)
     soup = bs4.BeautifulSoup(html.text, 'html.parser')
     return soup
 
 
 # universal retrieval
-def getAll(url, soup):
+def getAll(url):
+    soup = getSoup(url)
     preimgsrc = []
     imgsrc = []
     imgtags = soup.select('img')
@@ -37,17 +65,25 @@ def getAll(url, soup):
     return imgsrc
 
 
-# retrieves imgsrc list of img URLS for 4chan
-def getChan(url, soup):
-    fileText = soup.select('div .fileText a')
-    imgsrc = []
-    for tag in fileText:
-        src = 'http:' + tag.get('href')
-        imgsrc.append(src)
-    return imgsrc
+def get4chan(url):
+    print("Identified as 4chan thread.")
+    soup = getSoup(url)
+    selection = soup.select('div .fileText a')
+    imgurls = []
+    for tag in selection:
+        href = 'http:' + tag.get('href')
+        imgurls.append(href)
+    return imgurls
 
 
-# sanitises file names for OS module
+def getImgur(url):
+    # imgur urls types:
+    # imgur.com/a/abcdef (multi, single)--'div .post-image a', tag.get('href')
+    # imgur.com/gallery/abcdef (multi, single)
+    # imgur.com/abcdef (single)
+    print("Identified as Imgur post.")
+
+
 def cleanName(filename):
     exts = ['.jpg', '.jpeg', '.png', '.gif', '.webm', '.tif', '.tiff', '.bmp', '.pbm', '.pgm', '.ppm', '.pnm', '.webp', '.svg']
     for ext in exts:
@@ -57,32 +93,43 @@ def cleanName(filename):
 
 
 # iterates over list to download images
-def download(imgsrc):
+def download(imgurls):
     dlcount = 1
-    for imgurl in imgsrc:
-        print("Downloading", str(dlcount), "of", str(len(imgsrc)))
-        imgdata = requests.get(imgurl)
-        filename = imgurl[imgurl.rfind('/') + 1:]
-        # catches errors with invalid file names, often trailing after the extension
-        filename = cleanName(filename)
-        imgfile = open(filename, 'wb')
-        for chunk in imgdata.iter_content(10000):
-            imgfile.write(chunk)
-        imgfile.close()
+    for imgurl in imgurls:
+        print("Downloading {}/{} @ {}".format(str(dlcount), str(len(imgurls)), imgurl))
+        data = requests.get(imgurl)
+        name = imgurl[imgurl.rfind('/') + 1:]
+        with open(name, 'wb') as imgfile:
+            for chunk in data.iter_content(10000):
+                imgfile.write(chunk)
         dlcount += 1
 
+def main(args):
+    if len(args) > 2:
+        multi = True
 
-# TODO make a downloadAll function for ease of use when importing this module
+    for url in args[1:]:
+        dirname = url[url.rfind('/') + 1:]
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        os.chdir(dirname)
 
-if __name__ == '__main__':
-    url = input('URL >>> ')
-    soup = genSoup(url)
-    print('Generating soup object... '),
-    if '4chan' in url:
-        print("Downloading with '4chan' function...")
-        imgsrc = getChan(url, soup)
-    else:
-        print("Downloading with universal function...")
-        imgsrc = getAll(url, soup)
-    download(imgsrc)
-    input("Done! Enter to exit.")
+        if 'http' in url:
+            None
+        elif 'www' in url:
+            url = 'http://' + url
+        else:
+            url = 'http://www.' + url
+        print("Downloading from", url)
+
+        if 'boards.4chan.org' in url:
+            download(get4chan(url))
+        elif 'imgur.com' in url:
+            download(getImgur(url))
+        elif 'reddit.com' in url:
+            (getReddit(url))
+        print("Downloads completed for {}.\n".format(url))
+        os.chdir('..')
+
+main(sys.argv)
+
